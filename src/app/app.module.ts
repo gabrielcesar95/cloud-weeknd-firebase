3b13909891ed2d90a56a813f9ddd971e53b56677
Gabriel Cesar Mello <gcesar95@icloud.com>
2023-06-05 13:08:56 -0300

    feat: ✨ new task component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,9 +15,10 @@ import { getAuth, provideAuth } from '@angular/fire/auth';
 import { HeaderComponent } from './components/header/header.component';
 import { TaskComponent } from './components/task/task.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { NewTaskComponent } from './components/new-task/new-task.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, TaskComponent, FooterComponent],
+  declarations: [AppComponent, HeaderComponent, TaskComponent, FooterComponent, NewTaskComponent],
   imports: [
     BrowserModule,
     provideFirebaseApp(() => initializeApp(environment.firebase)),
